eb0c18cb863e0d9075fcde50617cb19211a904ee
aurianworld <auriankelen@gmail.com>
2021-12-06 13:29:18 -0500

    Minor Changes

--- Code/common_regular_beat.py
+++ Code/common_regular_beat.py
@@ -2,7 +2,6 @@
 
 import os
 import numpy as np
-from numpy import genfromtxt
 
 dirpath = "/home/osboxes/Data/beat-per-beat/_data/processed/Regular Beat/"
 ext = ('.csv')
@@ -15,8 +14,8 @@ for file_1 in os.listdir(dirpath):
         for file_2 in os.listdir(dirpath):
             if file_2.endswith(ext) and file_1 != file_2 :
 
-                np_file_2 =genfromtxt(dirpath+file_2, delimiter=',')
+                np_file_2 =np.genfromtxt(dirpath+file_2, delimiter=',')
 
                 common_beat = np.intersect1d(np_file_1[:,1],np_file_2[:,1])
 
-                np.savetxt(dirpath+'/common_beat/'+file_1[:35]+'__and__'+file_2[:35]+'_commonn_beat.txt', common_beat, delimiter=",")
+                np.savetxt(dirpath+'/common_beat/'+file_1[:35]+'__and__'+file_2[:35]+'_commonn_beat.txt', common_beat, fmt='%.3f', delimiter=",")
