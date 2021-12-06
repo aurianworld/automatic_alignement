65643186df5ff409fb3f0f1f3443a5f8c19b350a
aurianworld <auriankelen@gmail.com>
2021-12-06 11:01:12 -0500

    Script for detection of common regular beat between two symphony

---
+++ Code/common_regular_beat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+from numpy import genfromtxt
+
+dirpath = "/home/osboxes/Data/beat-per-beat/_data/processed/Regular Beat/"
+ext = ('.csv')
+
+for file_1 in os.listdir(dirpath):
+    if file_1.endswith(ext):
+
+        np_file_1 =genfromtxt(dirpath+file_1, delimiter=',')
+
+        for file_2 in os.listdir(dirpath):
+            if file_2.endswith(ext) and file_1 != file_2 :
+
+                np_file_2 =genfromtxt(dirpath+file_2, delimiter=',')
+
+                common_beat = np.intersect1d(np_file_1[:,1],np_file_2[:,1])
+
+                np.savetxt(dirpath+'/common_beat/'+file_1[:35]+'__and__'+file_2[:35]+'_commonn_beat.txt', common_beat, delimiter=",")
