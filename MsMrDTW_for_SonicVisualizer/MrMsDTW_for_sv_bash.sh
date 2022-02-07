5ed9b65659fb25560821f8749619de9bbd8d461f
aurianworld <auriankelen@gmail.com>
2022-02-07 09:27:22 -0500

    Minor esthetic changes

--- MsMrDTW_for_SonicVisualizer/MrMsDTW_for_sv_bash.sh
+++ MsMrDTW_for_SonicVisualizer/MrMsDTW_for_sv_bash.sh
@@ -6,5 +6,4 @@
 source '/home/osboxes/miniconda3/etc/profile.d/conda.sh'
 conda activate mrmsdtw
 
-
 /home/osboxes/automatic_alignement/MsMrDTW_forSonicVisualiser/mrmsdtw_for_sv.py --audio_1 $1 --audio_2 $2 --config_path "/home/osboxes/automatic_alignement/MsMrDTW_for_SonicVisualizer/MrMsDTW_config.conf"
