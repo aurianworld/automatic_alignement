9f22eb4e68fe0bc1d67caeeb5dff63e933ab08b8
aurianworld <auriankelen@gmail.com>
2022-02-07 09:25:33 -0500

    Changing the name of the conda env

--- MsMrDTW_for_SonicVisualizer/MrMsDTW_for_sv_bash.sh
+++ MsMrDTW_for_SonicVisualizer/MrMsDTW_for_sv_bash.sh
@@ -4,7 +4,7 @@
 #conda init bash
 
 source '/home/osboxes/miniconda3/etc/profile.d/conda.sh'
-conda activate sdia-python
+conda activate mrmsdtw
 
 
 /home/osboxes/automatic_alignement/MsMrDTW_forSonicVisualiser/mrmsdtw_for_sv.py --audio_1 $1 --audio_2 $2 --config_path "/home/osboxes/automatic_alignement/MsMrDTW_for_SonicVisualizer/MrMsDTW_config.conf"
