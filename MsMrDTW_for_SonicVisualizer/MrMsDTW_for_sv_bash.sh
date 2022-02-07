0f43c3130203b160b5307abd7ce4dc08618f993c
aurianworld <auriankelen@gmail.com>
2022-02-07 09:24:52 -0500

    Folder with all the file for MrMsDTW for SV

---
+++ MsMrDTW_for_SonicVisualizer/MrMsDTW_for_sv_bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Making sure that we are using the good environement 
+#conda init bash
+
+source '/home/osboxes/miniconda3/etc/profile.d/conda.sh'
+conda activate sdia-python
+
+
+/home/osboxes/automatic_alignement/MsMrDTW_forSonicVisualiser/mrmsdtw_for_sv.py --audio_1 $1 --audio_2 $2 --config_path "/home/osboxes/automatic_alignement/MsMrDTW_for_SonicVisualizer/MrMsDTW_config.conf"
