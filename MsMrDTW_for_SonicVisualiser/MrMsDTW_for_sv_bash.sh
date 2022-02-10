39b0c44efc68158d2bf547b3a6ee94f0be176afb
aurianworld <auriankelen@gmail.com>
2022-02-10 13:45:32 -0500

    Changing the environment for testing

--- MsMrDTW_for_SonicVisualiser/MrMsDTW_for_sv_bash.sh
+++ MsMrDTW_for_SonicVisualiser/MrMsDTW_for_sv_bash.sh
@@ -4,13 +4,7 @@
 #conda init bash
 
 source '~/miniconda3/etc/profile.d/conda.sh'
-<<<<<<< HEAD
-conda activate mrmsdtw
-
-/home/osboxes/automatic_alignement/MsMrDTW_for_SonicVisualiser/mrmsdtw_for_sv.py --audio_1 $1 --audio_2 $2 --config_path "/home/osboxes/automatic_alignement/MsMrDTW_for_SonicVisualiser/MrMsDTW_config.conf"
-=======
 conda deactivate
 conda activate mrmsdtw
 
-python3 [YOUR_PATH]/MsMrDTW_for_SonicVisualiser/mrmsdtw_for_sv.py --audio_1 $1 --audio_2 $2 --config_path "[YOUR_PATH]/MsMrDTW_for_SonicVisualiser/MrMsDTW_config.conf"
->>>>>>> 24e5ee8b6e3ce5d03a9dee7561b11e0aefd4aefc
+/home/osboxes/automatic_alignement/MsMrDTW_for_SonicVisualiser/mrmsdtw_for_sv.py --audio_1 $1 --audio_2 $2 --config_path "/home/osboxes/automatic_alignement/MsMrDTW_for_SonicVisualiser/MrMsDTW_config.conf"
