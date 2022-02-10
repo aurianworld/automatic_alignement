167ee883f5168e3e00672e02b257f48da926443c
aurianworld <auriankelen@gmail.com>
2022-02-10 13:43:43 -0500

    Debugging install

--- MsMrDTW_for_SonicVisualiser/install.sh
+++ MsMrDTW_for_SonicVisualiser/install.sh
@@ -2,12 +2,14 @@
 
 # this is the install file for MsMrDTW_for_SonicVisualiser 
 # it should 
-# 1. detect where this folder lives and save that in $HERE
+# 1. detect where this folder lives and save that in $MY_NAME
 # -> this should be $0 
 MY_NAME=$0
 echo $MY_NAME
 
 MY_DIRECTORY=$(dirname $MY_NAME)
+MY_ROOT=$(id -un)
+echo $MY_ROOT
 echo $MY_DIRECTORY
 
 # 2. replace [YOUR_PATH] in various files 
@@ -19,24 +21,27 @@ echo sed -i "s|[YOUR_PATH]|${MY_DIRECTORY}|" "${MY_DIRECTORY}"/MrMsDTW_for_sv_ba
 sed -i 's|\[YOUR_PATH\]|'${MY_DIRECTORY}'|g' "${MY_DIRECTORY}"/MrMsDTW_for_sv_bash.sh
 
 # 3. edit sonic visualiser config file
-SV_CONFIG="~/.config/sonic-visualiser/Sonic Visualiser.conf"
+SV_CONFIG="/home/"$MY_ROOT"/.config/sonic-visualiser/Sonic Visualiser.conf" #FOR LINUX
+# SV_CONFIG="/Users/"$MY_ROOT"/.config/sonic-visualiser/Sonic Visualiser.conf" #FOR MAC
+
 sed -i "/\[Alignment\]/d" "${SV_CONFIG}"
 sed -i "/alignment\-program/d" "${SV_CONFIG}"
 sed -i "/alignment\-type/d" "${SV_CONFIG}"
 echo '[Alignment]
-alignment-program='"${MY_DIRECTORY}"'/mrmsdtw_for_sv.py
+alignment-program='"${MY_DIRECTORY}"'/MrMsDTW_for_sv_bash.sh
 alignment-type=external-program-alignment' >> "${SV_CONFIG}"
 
 # # 4. install anaconda
-# source '~/miniconda3/etc/profile.d/conda.sh'
-# conda create -n mrmsdtw2 python=3.7
+# source "/home/"$MY_ROOT"/miniconda3/etc/profile.d/conda.sh"
+# # source "/Users/"$MY_ROOT"/miniconda3/etc/profile.d/conda.sh" #FOR MAC
+# conda create -n mrmsdtw2 python=3.7 
 
 # # 5. install requirements 
 # conda deactivate
 # conda activate mrmsdtw2
-# pip install -r ${MY_DIRECTORY}/requirement_mrmsdtw.txt
+# pip install -r ${MY_DIRECTORY}/requirements.txt
 
 
 # # 6. chmod the align shell file to be executable
-# chmod +x ${MY_DIRECTORY}/mrmsdtw_for_sv.py
-# chmod +x ${MY_DIRECTORY}/MrMsDTW_for_sv_bash.sh
+# chmod +x ""${MY_DIRECTORY}"/mrmsdtw_for_sv.py"
+# chmod +x ""${MY_DIRECTORY}"/MrMsDTW_for_sv_bash.sh"
