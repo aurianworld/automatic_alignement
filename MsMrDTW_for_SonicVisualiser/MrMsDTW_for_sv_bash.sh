5c4c4acf63fa87ec419ee659e20b98044e8f6818
Daniel Wolff <wolffd.mail@googlemail.com>
2022-02-10 15:24:13 +0100

    Update MrMsDTW_for_sv_bash.sh
    replaces the chmod in the readme user setup

--- MsMrDTW_for_SonicVisualiser/MrMsDTW_for_sv_bash.sh
+++ MsMrDTW_for_SonicVisualiser/MrMsDTW_for_sv_bash.sh
@@ -3,7 +3,8 @@
 #Making sure that we are using the good environement 
 #conda init bash
 
-source '/home/osboxes/miniconda3/etc/profile.d/conda.sh'
+source '~/miniconda3/etc/profile.d/conda.sh'
+conda deactivate
 conda activate mrmsdtw
 
-[YOUR_PATH]/MsMrDTW_for_SonicVisualiser/mrmsdtw_for_sv.py --audio_1 $1 --audio_2 $2 --config_path "[YOUR_PATH]/MsMrDTW_for_SonicVisualiser/MrMsDTW_config.conf"
+python3 [YOUR_PATH]/MsMrDTW_for_SonicVisualiser/mrmsdtw_for_sv.py --audio_1 $1 --audio_2 $2 --config_path "[YOUR_PATH]/MsMrDTW_for_SonicVisualiser/MrMsDTW_config.conf"
