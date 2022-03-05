ff88304d3a6ae7af33e4341e51f7845503f4eddb
aurianworld <auriankelen@gmail.com>
2022-03-05 11:23:29 -0500

    Fixing indentation

--- src/Utilities/annotation_path_from_audio_path.py
+++ src/Utilities/annotation_path_from_audio_path.py
@@ -5,6 +5,7 @@ import os, re, argparse
 def annotation_from_audio(audio_path):
 
     #We get only the base name of the audio path
+    
     file_name = os.path.basename(audio_path)[:-4] #we don't take the file extension
     cropped = False
 
@@ -26,21 +27,26 @@ def annotation_from_audio(audio_path):
             pattern = file_name + r'.*' + cropped_beats  #We know that the annotation files have additional info after the file name 
             search = re.search(pattern,base_name)
             if search :
+                print(folder_path + search.group(0) + '.txt') #when used in terminal
                 return folder_path + search.group(0) + '.txt'
-            else :
-                return 'No annotation for this audio file'
+
+        else :
+            print('No annotation for this audio file')
+            return 'No annotation for this audio file'
     
     #If it is not cropped:  
     folder_path = '/home/osboxes/Desktop/Dataset/02_Beat_per_beat_annotation/01_Full_Symphonies/'
-    for file in os.listdir(folder_path):
-        
+
+    for file in os.listdir(folder_path):        
         base_name = os.path.basename(file)
         pattern = file_name + r'.*' 
         search = re.search(pattern,base_name)
         if search :
+            print(folder_path + search.group(0))
             return folder_path + search.group(0)
-        else :
-                return 'No annotation for this audio file'
+    
+    print('No annotation for this audio file')
+    return 'No annotation for this audio file'
 
 
 if __name__ == '__main__':
